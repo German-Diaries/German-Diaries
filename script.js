0382df0e8fc71ff8a775149ac09612c1011f7680
German-Diaries <86650616+German-Diaries@users.noreply.github.com>
2021-07-10 15:51:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
     $(".wrapper").addClass('main-active');
     $(window).scroll(() => {
         if (this.scrollY > 150) {
-            $(".main-col3").addClass('main-active');
+            $(".main-col2, .main-col3").addClass('main-active');
         }
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
@@ -18,12 +18,25 @@ $(document).ready(() => {
 
     // typing animation script
     var typed = new Typed(".typing" , {
-        strings: ["It Is the Most Spoken Native Language in Europe.","It Has Great Cultural Inheritance and its Contribution to Knowledge is Priceless.","Excellent Business Opportunities.","Study at Renowned German Universities.","It Is Not as Hard as It Is Made Out to Be."],
-        typeSpeed: 60,
-        backSpeed: 30,
+        strings: ["Learning<br>Is<br>Fun","Learnen<br>Macht<br>Spaß"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2" , {
+        strings: [],
+        typeSpeed: 200,
+        backSpeed: 70,
+        loop: true
+    });
+    var typed = new Typed(".typing-3" , {
+        strings: [],
+        typeSpeed: 200,
+        backSpeed: 60,
         loop: true
     });
 });
+
 let main = document.querySelector(".main-div");
 let bucket = document.querySelector(".bucket");
 let main1 = document.querySelector(".main-div1");
